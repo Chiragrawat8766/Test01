5b14a61a4ab1d4c046b27055df5f52c0214e1157
unknown <Chiragrawat8766@gmail.com>
2025-01-09 00:47:45 +0530

    burger

--- hungry.py
+++ hungry.py
@@ -2,5 +2,6 @@ hungry = input("are you hungry?")
 if hungry == 'yes':
     print("Eat Indian Samosa!")
     print("Eat Italian pizza!")
+    print("Eat American burger")
 else:
     print("Do your Homework!")
